eababe3801d982ac8835cf060d1371404f163a22
raj18106111 <89646824+raj18106111@users.noreply.github.com>
2021-10-18 01:48:04 +0530

    Update urls.py

--- Messenger/urls.py
+++ Messenger/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
 from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
