41be5caa0d0aeac69682eec28a6ed04ae2eb9eaf
raj18106111 <89646824+raj18106111@users.noreply.github.com>
2021-10-18 01:24:23 +0530

    Update urls.py

--- Messenger/urls.py
+++ Messenger/urls.py
@@ -6,4 +6,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('chatapp.urls')),
     path('accounts/',include('accounts.urls')),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
